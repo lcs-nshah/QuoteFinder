2f83d485b91fdfbe2db5490b9474e90b61d913f0
Neil Shah <nshah@lcs.on.ca>
2025-03-18 20:09:54 -0400

    Added a button so that we can load a new quote if desired.

--- QuoteFinder/Views/QuoteView.swift
+++ QuoteFinder/Views/QuoteView.swift
@@ -12,29 +12,58 @@ struct QuoteView: View {
     // MARK: Stored properties
     @State var viewModel = QuoteViewModel()
     
+    // Controls button visibility
+    @State var buttonOpacity = 0.0
+    
+    // Starts a timer to show button
+    @State var buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    
     // MARK: Computed properties
     var body: some View {
         VStack {
-            
-            // Show a joke if one exists
             if let currentQuote = viewModel.currentQuote {
-                
                 Group {
                     Text(currentQuote.quoteText ?? "")
                         .padding(.bottom, 100)
                     
                     Text(currentQuote.quoteAuthor ?? "")
- 
                 }
                 .font(.title)
                 .multilineTextAlignment(.center)
                 
+                Button {
+                    withAnimation {
+                        viewModel.currentQuote = nil
+                        buttonOpacity = 0.0
+                    }
+                    
+                    // Get a new quote
+                    Task {
+                        await viewModel.fetchQuote()
+                    }
+                    
+                    // Restart timer
+                    buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+                    
+                } label: {
+                    
+                    Text("Next Quote")
+                }
+                .buttonStyle(.borderedProminent)
+                .opacity(buttonOpacity)
+                .onReceive(buttonTimer) { _ in
+                    withAnimation {
+                        buttonOpacity = 1.0
+                    }
+                    
+                    // Stop the timer
+                    buttonTimer.upstream.connect().cancel()
+                }
             }
-            
         }
     }
 }
- 
+
 #Preview {
     QuoteView()
 }
